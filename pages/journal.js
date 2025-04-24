6f9723205eabc0c37e92a32f0f9f5ea092258d3d
easylog <easyloghilfe@gmail.com>
2025-04-24 09:30:26 +0200

    Delete pages directory

--- pages/journal.js
+++
@@ -1,9 +0,0 @@
-
-export default function Journal() {
-  return (
-    <div style={{ padding: '2rem' }}>
-      <h1>📝 Journal Demo</h1>
-      <p>Dies ist eine Demo-Seite ohne Login.</p>
-    </div>
-  );
-}
